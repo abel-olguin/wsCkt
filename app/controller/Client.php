0c898a91be67b685cbe2a84cd0dcdd7933e6ed71
iscariot696 <osmer19@hotmail.com>
2015-08-16 05:42:54 -0500

    change name from controller ws to client

--- app/controller/Client.php
+++ app/controller/Client.php
@@ -9,21 +9,23 @@
  */
 require_once('ParentController.php');
 
-class WebserviceController extends ParentController{
+class ClientController extends ParentController{
 
     public function __construct(){
         $this->load_model('Client');
     }
 
-    public function insert_post($input){
+    public function add_post($input){
+
         $data = ['email'        => $input['email'],
                  'commission'   => $input['commission'],
                  'public_k'     => $input['public_k'],
                  'private_k'    => $input['private_k'],
                  'status'       => 1];
+
         $client = $this->Client->insert($data);
         if($client){
-            echo json_encode(['status'=>'ok','data'=>$client]);
+            echo json_encode($client);
         }
     }
 }
